5d30a9ac2b877e784123a1fefacd595dc2e036aa
Drew Tayman <dtayman@breachrx.com>
2020-08-13 12:49:52 -0400

    adding aws sdk

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import AWS from 'aws-sdk';
+
+// you shouldn't hardcode your keys in production! See http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
+AWS.config.update({accessKeyId: 'akid', secretAccessKey: 'secret', region:"us-east-2"});
+
+
+
 function App() {
+  let lambda = new AWS.Lambda({
+    endpoint: 'http://localhost:4566'
+  });
+
+  var params = {
+    FunctionName: 'helloworld-local-hello', /* required */
+    Payload: ""
+  };
+
+  lambda.invoke(params, function(err, data) {
+
+    if (err) console.log(err, err.stack); // an error occurred
+
+    else     alert(data.Payload);           // successful response
+
+  });
   return (
     <div className="App">
       <header className="App-header">
